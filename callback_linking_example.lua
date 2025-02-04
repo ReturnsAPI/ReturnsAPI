6ed4b9cd02d389f8039bfbb9c61a254946facb78
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-02-04 12:54:23 -0500

    Update callback_linking_example.lua

--- callback_linking_example.lua
+++ callback_linking_example.lua
@@ -6,9 +6,9 @@
 {
     onHitProc = function(self, fn)
         -- item:onHitProc calls Callback.onHitProc:add
-        
+
         -- Add closure that runs fn if item_count > 0
-        -- Callback ID is returned; item callback can be removed via Callback.onHitProc:remove(id)
+        -- Callback ID is returned; item callback can be removed via Callback.remove(id)
         return Callback.onHitProc:add(function(cb_args)
             if actor:item_count(self.value) > 0 then
                 fn(cb_args)
