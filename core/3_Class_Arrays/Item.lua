7c875daaeee8917eff8dd9c800e2dfc5a73efebf
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-02-11 13:36:56 -0500

    :create now returns instance

--- core/3_Class_Arrays/Item.lua
+++ core/3_Class_Arrays/Item.lua
@@ -32,27 +32,49 @@ end
 
 methods_class[rapi_name] = {
 
+    -- create = function(self, x, y, target)
+    --     local object_id = self.object_id
+    --     if object_id == nil
+    --     or object_id == -1 then return nil end
+
+    --     gm.item_drop_object(object_id, x, y, Wrap.unwrap(target), false)
+
+    --     -- Look for drop (because gm.item_drop_object does not actually return the instance for some reason)
+    --     -- The drop spawns 40 px above y parameter
+    --     local drop = nil
+    --     local drops = Instance.find_all(gm.constants.pPickupItem) --, gm.constants.oCustomObject_pPickupItem)
+    --     for _, d in ipairs(drops) do
+    --         if math.abs(d.x - x) <= 1 and math.abs(d.y - (y - 40)) <= 1 then
+    --             drop = d
+    --             d.y = d.y + 40
+    --             d.ystart = d.y
+    --             break
+    --         end
+    --     end
+
+    --     return drop
+    -- end
+
+
     create = function(self, x, y, target)
         local object_id = self.object_id
         if object_id == nil
         or object_id == -1 then return nil end
 
-        gm.item_drop_object(object_id, x, y, Wrap.unwrap(target), false)
+        -- This function spawns the item 40 px above, so add 40 to y in the call
+        gm.item_drop_object(object_id, x, y + 40, Wrap.unwrap(target), false)
 
         -- Look for drop (because gm.item_drop_object does not actually return the instance for some reason)
-        -- The drop spawns 40 px above y parameter
-        -- local drop = nil
-        -- local drops = Instance.find_all(gm.constants.pPickupItem, gm.constants.oCustomObject_pPickupItem)
-        -- for _, d in ipairs(drops) do
-        --     if math.abs(d.x - x) <= 1.0 and math.abs(d.y - (y - 40.0)) <= 1.0 then
-        --         drop = d
-        --         d.y = d.y + 40.0
-        --         d.ystart = d.y
-        --         break
-        --     end
-        -- end
+        local drop = nil
+        local drops = Instance.find_all(gm.constants.pPickupItem) --, gm.constants.oCustomObject_pPickupItem)   -- TODO
+        for _, d in ipairs(drops) do
+            if math.abs(d.x - x) <= 1 and math.abs(d.y - y) <= 1 then
+                drop = d
+                break
+            end
+        end
 
-        -- return drop
+        return drop
     end
 
 }
