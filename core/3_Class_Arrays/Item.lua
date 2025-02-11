ed55f77d7b2adab7f6a8a403f0c00eb9b87c0e8f
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-02-11 13:38:32 -0500

    Update Item.lua

--- core/3_Class_Arrays/Item.lua
+++ core/3_Class_Arrays/Item.lua
@@ -32,30 +32,6 @@ end
 
 methods_class[rapi_name] = {
 
-    -- create = function(self, x, y, target)
-    --     local object_id = self.object_id
-    --     if object_id == nil
-    --     or object_id == -1 then return nil end
-
-    --     gm.item_drop_object(object_id, x, y, Wrap.unwrap(target), false)
-
-    --     -- Look for drop (because gm.item_drop_object does not actually return the instance for some reason)
-    --     -- The drop spawns 40 px above y parameter
-    --     local drop = nil
-    --     local drops = Instance.find_all(gm.constants.pPickupItem) --, gm.constants.oCustomObject_pPickupItem)
-    --     for _, d in ipairs(drops) do
-    --         if math.abs(d.x - x) <= 1 and math.abs(d.y - (y - 40)) <= 1 then
-    --             drop = d
-    --             d.y = d.y + 40
-    --             d.ystart = d.y
-    --             break
-    --         end
-    --     end
-
-    --     return drop
-    -- end
-
-
     create = function(self, x, y, target)
         local object_id = self.object_id
         if object_id == nil
