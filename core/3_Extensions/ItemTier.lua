214125d29de203764c687f89c1ae5f9c33264700
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-02-17 13:14:41 -0500

    Update ItemTier.lua

--- core/3_Extensions/ItemTier.lua
+++ core/3_Extensions/ItemTier.lua
@@ -30,23 +30,34 @@ end
 
 ItemTier.new = function(namespace, identifier)
     local tiers_array = Array.wrap(gm.variable_global_get("item_tiers"))
+    local loot_pools_array = Array.wrap(gm.variable_global_get("treasure_loot_pools"))
     new_index = #tiers_array
 
-    -- Create new struct for tier
+    -- Create new tier struct for tier
     local tier_struct = gm.struct_create()
     tier_struct.index                       = new_index
     tier_struct.text_color                  = "w"
     tier_struct.pickup_color                = Color.WHITE
     tier_struct.pickup_color_bright         = Color.WHITE
-    tier_struct.item_pool_for_reroll        = 0     -- TODO add pool
-    tier_struct.equipment_pool_for_reroll   = 0     -- TODO add pool
+    tier_struct.item_pool_for_reroll        = new_index
+    tier_struct.equipment_pool_for_reroll   = new_index
     tier_struct.ignore_fair                 = false
     tier_struct.fair_item_value             = 1
     tier_struct.pickup_particle_type        = -1
     tier_struct.spawn_sound                 = 57
 
-    -- Push onto `item_tiers` array
+    -- Create new loot pool struct for each tier
+    local loot_struct = gm.struct_create()
+    loot_struct.index                       = new_index
+    loot_struct.item_tier                   = new_index
+    loot_struct.drop_pool                   = gm.ds_list_create()
+    loot_struct.available_drop_pool         = gm.ds_list_create()
+    loot_struct.is_equipment_pool           = false
+    loot_struct.command_crate_object_id     = 800   -- White crate
+
+    -- Push onto arrays
     tiers_array:push(tier_struct)
+    loot_pools_array:push(loot_struct)
 
     -- Add to find table
     if not item_tier_find_table[namespace] then item_tier_find_table[namespace] = {} end
