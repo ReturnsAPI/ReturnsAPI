255a58d28c6281033fb2f2d6ae19b35154a5b30d
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-10 19:09:41 -0400

    Moved Struct getset to its own mt

--- core/2_General/HitInfo.lua
+++ core/2_General/HitInfo.lua
@@ -26,8 +26,6 @@ methods_hitinfo = {
 
 -- ========== Metatables ==========
 
-local out = gmf.rvalue_new(0)
-
 metatable_hitinfo = {
     __index = function(t, k)
         -- Get wrapped value
@@ -39,42 +37,14 @@ metatable_hitinfo = {
             return methods_hitinfo[k]
         end
 
-        ---- Getter
-        local holder = ffi.new("struct RValue[2]")  -- args holder
-
-        -- hitinfo
-        holder[0] = ffi.new("struct RValue")
-        holder[0].type = 6
-        holder[0].yy_object_base = Proxy.get(t)
-
-        -- key
-        holder[1] = gmf.rvalue_new_string(k)
-
-        gmf.variable_struct_get(out, nil, nil, 2, holder)
-        local ret_val, is_instance_id = rvalue_to_lua(out)
-        if is_instance_id then ret_val = gm.CInstance.instance_id_to_CInstance[ret_val] end
-        return Wrap.wrap(ret_val)
+        -- Getter
+        return metatable_struct_getset.__index(t, k)
     end,
 
 
     __newindex = function(t, k, v)
-        ---- Setter
-        local holder = ffi.new("struct RValue[3]")  -- args holder
-
-        -- hitinfo
-        holder[0] = ffi.new("struct RValue")
-        holder[0].type = 6
-        holder[0].yy_object_base = Proxy.get(t)
-
-        -- key
-        holder[1] = gmf.rvalue_new_string(k)
-
-        -- value
-        v = Wrap.unwrap(v)
-        if type(v) == "string" then holder[2] = gmf.rvalue_new_string(v)
-        else holder[2] = gmf.rvalue_new(v) end
-
-        gmf.variable_struct_set(out, nil, nil, 3, holder)
+        -- Setter
+        return metatable_struct_getset.__newindex(t, k, v)
     end,
 
     
