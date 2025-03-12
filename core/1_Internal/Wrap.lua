4c4b87eff401524df4f4912acb65037af2edb4ef
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-12 00:43:15 -0400

    Update Wrap.lua

--- core/1_Internal/Wrap.lua
+++ core/1_Internal/Wrap.lua
@@ -6,13 +6,19 @@ Wrap = new_class()
 
 -- ========== Static Methods ==========
 
+local rvalue_type_lookup = {
+    Array       = RValue.Type.ARRAY,
+    Struct      = RValue.Type.OBJECT,
+
+    Instance    = RValue.Type.REF,
+}
+
 -- May return an RValue.Type as a second value
 -- Make sure this gets passed into RValue.new if using that; having them be on the same line works
 Wrap.unwrap = function(value)
     local rvalue_type = nil
-    if type(value) == "table" then
-        if      value.RAPI == "Instance"    then rvalue_type = RValue.Type.REF
-        end
+    if type(value) == "table" and value.RAPI then
+        rvalue_type = rvalue_type_lookup[value.RAPI]
     end
     return Proxy.get(value) or value, rvalue_type
 end
