0c12e1fbf2eca6917c3b902e0e07179f5185304b
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-11 22:23:04 -0400

    Update Wrap.lua

--- core/1_Internal/Wrap.lua
+++ core/1_Internal/Wrap.lua
@@ -6,6 +6,8 @@ Wrap = new_class()
 
 -- ========== Static Methods ==========
 
+-- May return an RValue.Type as a second value
+-- Make sure this gets passed into RValue.new if using that; having them be on the same line works
 Wrap.unwrap = function(value)
     local rvalue_type = nil
     if type(value) == "table" then
