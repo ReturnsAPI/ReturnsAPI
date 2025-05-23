991dd91c284905ecb369adb3fc57c48ec3ee0ef8
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-05-23 18:51:51 -0400

    Create Vector.lua

---
+++ core/0_Internal/Vector.lua
@@ -0,0 +1,117 @@
+-- Vector
+
+-- A simple 2D vector library
+
+Vector = new_class()
+
+
+
+-- ========== Metatables ==========
+
+local wrapper_name = "Vector"
+
+make_table_once("metatable_vector_class", {
+    __call = function(t, x, y)
+        -- Clone
+        if type(x) == "table" then
+            return setmetatable({
+                x = x.x,
+                y = x.y,
+            }, metatable_vector)
+        end
+
+        -- New
+        return setmetatable({
+            x = x or 0,
+            y = y or 0
+        }, metatable_vector)
+    end,
+
+
+    __metatable = "RAPI.Class."..wrapper_name
+})
+setmetatable(Vector, metatable_vector_class)
+
+
+make_table_once("metatable_vector", {
+    __index = function(vec, k)
+        -- Get length/direction
+        if k == "length"    then return  math.sqrt(vec.x^2, vec.y^2)                end
+        if k == "direction" then return -math.atan2(vec.y, vec.x) * Math.RAD2DEG    end
+    end,
+
+
+    __newindex = function(vec, k, v)
+        -- Set length
+        if k == "length" then
+            local length = math.sqrt(vec.x^2 + vec.y^2)
+            if length == 0 then return end
+            local scale = v / length
+            vec.x = vec.x * scale
+            vec.y = vec.y * scale
+            return
+        end
+
+        -- Set direction
+        if k == "direction" then
+            local length = math.sqrt(vec.x^2 + vec.y^2)
+            vec.x =  math.cos(v * Math.DEG2RAD) * length
+            vec.y = -math.sin(v * Math.DEG2RAD) * length
+            return
+        end
+    end,
+    
+    
+    __len = function(vec)
+        -- Get length
+        return math.sqrt(vec.x^2, vec.y^2)
+    end,
+
+
+    __eq = function(v1, v2)
+        -- Check equivalence
+        return (v1.x == v2.x) and (v1.y == v2.y)
+    end,
+
+
+    __add = function(v1, v2)
+        -- Add
+        return Vector.new(v1.x + v2.x, v1.y + v2.y)
+    end,
+
+
+    __sub = function(v1, v2)
+        -- Subtract
+        return Vector.new(v1.x - v2.x, v1.y - v2.y)
+    end,
+
+
+    __mul = function(vec, value)
+        -- Dot product
+        if type(value) == "table" then return Vector.new(vec.x * value.x, vec.y * value.y) end
+
+        -- Scalar multiply
+        return Vector.new(vec.x * value, vec.y * value)
+    end,
+
+
+    __div = function(vec, value)
+        -- Scalar divide
+        return Vector.new(vec.x / value, vec.y / value)
+    end,
+
+
+    __unm = function(vec)
+        -- Negate
+        return Vector.new(-vec.x, -vec.y)
+    end,
+
+
+    __metatable = "RAPI.Wrapper."..wrapper_name
+})
+
+
+
+-- Public export
+__class.Vector = Vector
+__class_mt.Vector = metatable_vector_class
