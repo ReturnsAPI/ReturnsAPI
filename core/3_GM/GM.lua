15477d0e70dbd186f4047698e446f683ea78a4d7
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-09-01 18:05:00 -0400

    Bring back GM and Global

---
+++ core/3_GM/GM.lua
@@ -0,0 +1,114 @@
+-- GM
+
+--[[
+This class is a version of `gm` that automatically
+wraps/unwraps values with ReturnsAPI wrappers.
+
+E.g.,
+```lua
+GM.instance_create(x, y, Object.find("lizard"))    --> returns an Actor wrapper
+```
+
+If you need to pass a struct/instance into `self`/`other`, use `GM.SO`.
+
+E.g.,
+```lua
+-- The first two arguments are `self, other`
+GM.SO.recalculate_stats(actor, actor)
+```
+]]
+
+GM = new_class()
+GM_callso = new_class()
+
+
+
+-- ========== Functions ==========
+
+-- Loop through constants and
+-- add all scripts to GM
+for fn, _ in pairs(gm.constants) do
+    local type_ = gm.constant_types[fn]
+    if type_ == "script" or type_ == "gml_script" then
+
+        -- Normal
+        GM[fn] = function(...)
+            local args = table.pack(...)
+
+            -- Unwrap args
+            for i = 1, args.n do
+                args[i] = Wrap.unwrap(args[i])
+            end
+
+            return Wrap.wrap(gm.call(fn, nil, nil, table.unpack(args)))
+        end
+
+        -- self/other
+        GM_callso[fn] = function(self, other, ...)
+            local args = table.pack(...)
+
+            -- Unwrap args
+            for i = 1, args.n do
+                args[i] = Wrap.unwrap(args[i])
+            end
+
+            return Wrap.wrap(gm.call(fn, Wrap.unwrap(self, true), Wrap.unwrap(other, true), table.unpack(args)))
+        end
+
+    end
+end
+
+
+
+-- ========== Metatables ==========
+
+make_table_once("metatable_GM", {
+    __index = function(t, k)
+        if k == "SO" then return GM_callso end
+        return t[k]
+    end,
+
+
+    __newindex = function(t, k, v)
+        log.error("GM has no properties to set", 2)
+    end,
+
+
+    __metatable = "RAPI.Class.GM"
+})
+setmetatable(GM, metatable_GM)
+
+
+make_table_once("metatable_callso", {
+    __index = function(t, k)
+        return t[k]
+    end,
+
+
+    __newindex = function(t, k, v)
+        log.error("GM.SO has no properties to set", 2)
+    end,
+
+
+    __metatable = "RAPI.Class.GM.SO"
+})
+setmetatable(GM_callso, metatable_callso)
+
+
+
+-- Public export
+__class.GM = GM
+__class_mt.GM = metatable_GM
+
+
+
+-- ========== Notable Functions ==========
+
+--@section Notable Functions
+
+--[[
+**Actors**
+Function | Arguments | Description
+| - | - | -
+`actor_heal_barrier( actor, amount )` | `actor` - The actor to grant to. <br>`amount` - The amount of barrier to grant. | Grants barrier to the actor. <br>Automatically capped by the actor's `maxbarrier`.
+]]
---
+++ core/3_GM/Global.lua
@@ -0,0 +1,60 @@
+-- Global
+
+--[[
+Allows for accessing global variables via `Global.<variable>`
+]]
+
+Global = new_class()
+
+run_once(function()
+    __global_cache = {}
+end)
+
+
+
+-- ========== Internal ==========
+
+Global.internal.initialize = function()
+    -- Cache for some globals; add to it if needed
+    make_table_once("__global_cache", {
+        __mtd_deserialize                   = Map.wrap(GM.variable_global_get("__mtd_deserialize")),
+        artifact_cognation_enemy_blacklist  = Map.wrap(GM.variable_global_get("artifact_cognation_enemy_blacklist")),
+        custom_object                       = GM.variable_global_get("custom_object"),
+        environment_log_display_list        = List.wrap(GM.variable_global_get("environment_log_display_list")),
+        item_log_display_list               = List.wrap(GM.variable_global_get("item_log_display_list")),
+        item_tiers                          = GM.variable_global_get("item_tiers"),
+        stage_progression_order             = GM.variable_global_get("stage_progression_order"),
+        treasure_loot_pools                 = GM.variable_global_get("treasure_loot_pools"),
+    })
+end
+
+
+
+-- ========== Metatables ==========
+
+metatable_global = {
+    __index = function(t, k)
+        -- Check cache
+        if __global_cache[k] then return __global_cache[k] end
+        
+        return Wrap.wrap(gm.variable_global_get(k))
+    end,
+
+
+    __newindex = function(t, k, v)
+        -- Prevent setting any global that is in cache
+        if __global_cache[k] then log.error("Do not set global variable '"..k.."'", 2) end
+        
+        gm.variable_global_set(k, Wrap.unwrap(v, true))
+    end,
+
+
+    __metatable = "RAPI.Class.Global"
+}
+setmetatable(Global, metatable_global)
+
+
+
+-- Public export
+__class.Global = Global
+__class_mt.Global = metatable_global
