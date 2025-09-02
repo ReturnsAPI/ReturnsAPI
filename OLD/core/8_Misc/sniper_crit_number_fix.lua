8cf892e762077d2d47a9dc833c2cbc6e7e60282f
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-09-02 16:05:33 -0400

    Delete OLD

--- OLD/core/8_Misc/sniper_crit_number_fix.lua
+++
@@ -1,9 +0,0 @@
--- Fix incorrect damage numbers related to Sniper's spotter drone
--- Somehow fixes it for mp too
-
-local ptr = gm.get_script_function_address(gm.constants.damager_calculate_damage)   -- 0x0000000 140B154D0
-
-memory.dynamic_hook_mid("RAPI.Fix.sniper_crit_number", {"rbp+20h"}, {"RValue*"}, 0, ptr:add(0x6A0), function(args)
-    -- `rbp+20h` is `damage_fake`
-    args[1].value = args[1].value * 2
-end)
--- OLD/core/Wrappers.md
+++
@@ -1,3 +0,0 @@
-Array
-- Input     
-- Output    
