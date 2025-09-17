14ca785d4b98de1d54833fc83efea78a61885e6f
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-09-16 23:45:50 -0400

    Update CallbackCache.lua

--- core/1_Internal/CallbackCache.lua
+++ core/1_Internal/CallbackCache.lua
@@ -150,7 +150,7 @@ run_once(function()
                     local priority_table = section_table[priority]
 
                     -- Loop through priority table
-                    -- and add data tables to list
+                    -- and call function on data table
                     for _, fn_table in ipairs(priority_table) do
                         fn(fn_table)
                     end
