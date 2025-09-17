cbc813c45e2bfb878b9b46177ec74d7622657264
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-09-17 12:28:08 -0400

    TODO

--- core/2_CacheStructures/FindCache.lua
+++ core/2_CacheStructures/FindCache.lua
@@ -20,18 +20,29 @@ run_once(function()
         __index = {
 
             -- Set value in cache
-            set = function(self, value, identifier, namespace)
+            -- Can optionally provide a unique numerical ID
+            --      * Used by ItemTier and LootPool for example
+            set = function(self, value, identifier, namespace, num_id)
                 -- Create namespace cache if existn't
                 self[namespace] = self[namespace] or {}
 
                 -- Set value
                 self[namespace][identifier] = value
+
+                -- Set value keyed to a numerical ID
+                if num_id then self[num_id] = value end
             end,
 
 
             -- Get value from cache in provided namespace,
             -- or default namespace *and* "ror" if not provided
+            -- Alternatively, get value using numerical ID
             get = function(self, identifier, namespace, namespace_is_specified)
+                -- Get value from cache using numerical ID
+                if type(identifier) == "number" then
+                    return self[identifier]
+                end
+
                 -- Create namespace cache if existn't
                 self[namespace] = self[namespace] or {}
 
