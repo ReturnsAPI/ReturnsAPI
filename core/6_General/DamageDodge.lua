197981c5ad179e1e334890816f87d2da2821318f
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-09-24 13:32:43 -0400

    Update DamageDodge.lua

--- core/6_General/DamageDodge.lua
+++ core/6_General/DamageDodge.lua
@@ -2,3 +2,4 @@
 
 -- Midhook `damage_get_dodge` after line 20
 -- Allow the user to force trigger a dodge with any damage popup word (IMMUNE, INVINCIBLE, BLOCK, or PARRY)
+    -- In each callback function, should inform them if a dodge has already happened (so multiple effects don't get consumed at once)
