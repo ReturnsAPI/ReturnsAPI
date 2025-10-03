261157b73f67a8b5f78bfd06d857f819dfd95cff
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-10-03 08:22:40 -0400

    Tracer : Update to use constructor, and add docs

--- core/6_General/Tracer.lua
+++ core/6_General/Tracer.lua
@@ -1,12 +1,12 @@
 -- Tracer
 
--- TODO: Rewrite with actual TracerKindInfo constructor
+-- TODO: Add property docs
 
 Tracer = new_class()
 
 run_once(function()
     __tracer_find_table = FindCache.new()
-    __tracer_funcs = {}
+    __tracer_callbacks = {}
 end)
 
 
@@ -85,6 +85,41 @@ end
 
 
 
+-- ========== Internal ==========
+
+Tracer.internal.initialize = function()
+    -- Populate find table with vanilla tracers
+    for constant, id in pairs(tracer_constants) do
+        local namespace = "ror"
+        local identifier = constant:lower()
+
+        local struct = Global.tracer_info:get(id)
+
+        -- Custom properties
+        struct.namespace    = namespace
+        struct.identifier   = identifier
+
+        __tracer_find_table:set(
+            {
+                wrapper = Tracer.wrap(id),
+                struct  = struct
+            },
+            identifier, namespace, id
+        )
+    end
+
+    -- Update cached wrappers
+    __tracer_find_table:loop_and_update_values(function(value)
+        return {
+            wrapper = Tracer.wrap(value.wrapper),
+            struct  = value.struct
+        }
+    end)
+end
+table.insert(_rapi_initialize, Tracer.internal.initialize)
+
+
+
 -- ========== Properties ==========
 
 --@section Properties
@@ -95,48 +130,87 @@ Property | Type | Description
 | - | - | -
 `value`         | number    | *Read-only.* The ID of the tracer.
 `RAPI`          | string    | *Read-only.* The wrapper name.
+
+<br>
+
+Property | Type | Description
+| - | - | -
+`namespace`                         | string    | The namespace the tracer is in.
+`identifier`                        | string    | The identifier for the tracer within the namespace.
+`consistent_sparks_flip`            | bool      | 
+`show_sparks_if_miss`               | bool      | 
+`sparks_offset_y`                   | number    | 
+`show_end_sparks_on_piercing_hit`   | bool      | 
+`override_sparks_miss`              | number    | 
+`override_sparks_solid`             | number    | 
+`draw_tracer`                       | bool      | 
 ]]
 
 
 
 -- ========== Static Methods ==========
 
+--@section Static Methods
+
+--@static
+--@return   Tracer
+--@param    identifier      | string    | The identifier for the tracer.
+--[[
+Creates a new tracer with the given identifier if it does not already exist,
+or returns the existing one if it does.
+]]
 Tracer.new = function(NAMESPACE, identifier)
+    -- Return existing tracer if found
     local tracer = Tracer.find(identifier, NAMESPACE)
     if tracer then return tracer end
 
+    -- Get next usable ID for tracer
     local tracer_info_array = Global.tracer_info
-    local index = #tracer_info_array
+    local id = #tracer_info_array
 
-    local struct = Struct.new{
-        namespace                       = NAMESPACE,    -- RAPI custom variable
-        identifier                      = identifier,   -- RAPI custom variable
-        consistent_sparks_flip          = false,
-        show_sparks_if_miss             = true,
-        sparks_offset_y                 = 0,
-        show_end_sparks_on_piercing_hit = false,
-        override_sparks_miss            = -1,
-        override_sparks_solid           = -1,
-        draw_tracer                     = true
-    }
+    local struct = Struct.new(
+        gm.constants.TracerKindInfo
 
+        -- Default constructor args
+        -- consistent_sparks_flip           false
+        -- show_sparks_if_miss              true
+        -- sparks_offset_y                  0
+        -- show_end_sparks_on_piercing_hit  false
+        -- override_sparks_miss             -1
+        -- override_sparks_solid            -1
+        -- draw_tracer                      true
+    )
+
+    -- Custom properties
+    struct.namespace    = NAMESPACE
+    struct.identifier   = identifier
+
+    -- Push onto array
     tracer_info_array:push(struct)
 
-    local wrapper = Tracer.wrap(index)
+    local tracer = Tracer.wrap(id)
 
     -- Add to find table
     __tracer_find_table:set(
         {
-            wrapper = wrapper,
+            wrapper = tracer,
             struct  = struct
         },
-        identifier, NAMESPACE, index
+        identifier, NAMESPACE, id
     )
 
-    return wrapper
+    return tracer
 end
 
 
+--@static
+--@return       Tracer or nil
+--@param        identifier  | string    | The identifier to search for.
+--@optional     namespace   | string    | The namespace to search in.
+--[[
+Searches for the specified tracer and returns it.
+If no namespace is provided, searches in your mod's namespace first, and vanilla tracers second.
+]]
 Tracer.find = function(identifier, namespace, namespace_is_specified)
     -- Check in find table
     local cached = __tracer_find_table:get(identifier, namespace, namespace_is_specified)
@@ -146,6 +220,12 @@ Tracer.find = function(identifier, namespace, namespace_is_specified)
 end
 
 
+--@static
+--@return       Tracer
+--@param        tracer      | number    | The tracer to wrap.
+--[[
+Returns a Tracer wrapper containing the provided tracer.
+]]
 Tracer.wrap = function(tracer)
     -- Input:   number or Tracer wrapper
     -- Wraps:   number
@@ -175,8 +255,8 @@ methods_tracer = {
     --[[
     Sets the function that gets called whenever the tracer spawns.
     ]]
-    set_func = function(self, func)
-        __tracer_funcs[self.value] = func
+    set_callback = function(self, func)
+        __tracer_callbacks[self.value] = func
     end
 
 }
@@ -226,8 +306,8 @@ make_table_once("metatable_tracer", {
 gm.post_script_hook(gm.constants.bullet_draw_tracer, function(self, other, result, args)
     local tracer_kind = args[1].value
 
-    -- don't waste time on vanilla tracers
-    if tracer_kind <= tracer_constants.PLAYER_DRONE then return end
+    local fn = __tracer_callbacks[tracer_kind]
+    if not fn then return end
 
     local tracer_col = args[2].value
     local x1 = args[3].value
@@ -235,10 +315,7 @@ gm.post_script_hook(gm.constants.bullet_draw_tracer, function(self, other, resul
     local x2 = args[5].value
     local y2 = args[6].value
 
-    local fn = __tracer_funcs[tracer_kind]
-    if fn then
-        fn(x1, y1, x2, y2, tracer_col)
-    end
+    fn(x1, y1, x2, y2, tracer_col)
 end)
 
 
