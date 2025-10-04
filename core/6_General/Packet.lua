4585ca453d53dd64f66a716bd7dcd6e7befa6f6c
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-10-04 10:37:57 -0400

    Update Packet.lua

--- core/6_General/Packet.lua
+++ core/6_General/Packet.lua
@@ -58,7 +58,8 @@ methods_packet = {
     --@param        deserializer    | function  | The deserialization function.
     --[[
     Set the serialization and deserialization functions for the packet.
-    The arguments for each are `buffer, player`.
+    The arguments for the serializer are `buffer, <variable number of arguments>`.
+    The arguments for the deserializer are `buffer, player`.
     ]]
     set_serializers = function(self, serializer, deserializer)
         __callbacks_onSerialize[self.value] = serializer
