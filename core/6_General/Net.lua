fc295de0dc9874ec31752f49333424aab89a025c
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-11-06 09:20:37 -0500

    Net : Cache net status when entering a run

--- core/6_General/Net.lua
+++ core/6_General/Net.lua
@@ -11,6 +11,8 @@ Net.client   --> `true` if the game client is currently a lobby client.
 
 Net = new_class()
 
+__net_cache = __net_cache or {}
+
 
 
 -- ========== Metatables ==========
@@ -19,9 +21,9 @@ local wrapper_name = "Net"
 
 make_table_once("metatable_net_class", {
     __index = function(t, k)
-        if k == "online" then return gm._mod_net_isOnline() end
-        if k == "host"   then return gm._mod_net_isHost() end
-        if k == "client" then return gm._mod_net_isClient() end
+        if k == "online" then return __net_cache.online or gm._mod_net_isOnline()   end
+        if k == "host"   then return __net_cache.host   or gm._mod_net_isHost()     end
+        if k == "client" then return __net_cache.client or gm._mod_net_isClient()   end
     end,
 
 
@@ -33,6 +35,23 @@ make_table_once("metatable_net_class", {
     __metatable = "RAPI.Class."..wrapper_name
 })
 setmetatable(Net, metatable_net_class)
+
+
+
+-- ========== Hooks ==========
+
+Hook.add_pre(RAPI_NAMESPACE, gm.constants.run_create, Callback.internal.FIRST, function(self, other, result, args)
+    __net_cache = {
+        online  = gm._mod_net_isOnline(),
+        host    = gm._mod_net_isHost(),
+        client  = gm._mod_net_isClient()
+    }
+end)
+
+
+Hook.add_post(RAPI_NAMESPACE, gm.constants.run_destroy, Callback.internal.FIRST, function(self, other, result, args)
+    __net_cache = {}
+end)
 
 
 
