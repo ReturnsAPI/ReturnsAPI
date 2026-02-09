638e07195a098952660122ef281885a92183db02
Klehrik <78520710+Klehrik@users.noreply.github.com>
2026-02-09 16:54:57 -0500

    Update store_stage_variant.lua

--- core/9_Misc/store_stage_variant.lua
+++ core/9_Misc/store_stage_variant.lua
@@ -1,6 +1,13 @@
 -- Store current stage variant as a global variable
+
+gm.variable_global_set("stage_variant", -1)
+
 -- Hooks line 33 (to get value of `arg1` at some point)
 local ptr = gm.get_script_function_address(gm.constants.stage_goto)
 memory.dynamic_hook_mid("saveMod.stage_goto", {"rbp-D0h"}, {"RValue*"}, 0, ptr:add(0x1108), function(args)
     gm.variable_global_set("stage_variant", args[1].value)
+end)
+
+gm.post_script_hook(gm.constants.run_destroy, function(self, other, result, args)
+    gm.variable_global_set("stage_variant", -1)
 end)
