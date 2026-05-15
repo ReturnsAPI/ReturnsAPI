974c69fcac27e46ef274a0522cb54f044c5f2783
Klehrik <78520710+Klehrik@users.noreply.github.com>
2026-05-15 13:59:42 -0400

    Actor and Player base

--- core/g_instances/Player.lua
+++ core/g_instances/Player.lua
@@ -1,114 +1,53 @@
-if __DEACTIVATE_OLD then return end
 -- Player
 
---[[
-Player wrappers are "children" of both @link {`Instance` | Instance} and @link {`Actor` | Actor}, and can use their properties and instance methods.
-]]
-
+---@class Player: Actor
 Player = new_class()
-
-
-
--- ========== Properties ==========
-
---@section Properties
-
---[[
-**Wrapper**
-Property | Type | Description
-| - | - | -
-`value`/`cinstance` | CInstance     | *Read-only.* The `sol.CInstance*` of the Actor.
-`RAPI`              | string        | *Read-only.* The wrapper name.
-`id`                | number        | *Read-only.* The instance ID of the Actor.
-]]
-
+C.Player = Player
 
 
 -- ========== Static Methods ==========
 
---@section Static Methods
-
---@static
---@return       Player or Instance
 --[[
-Returns the Player instance of this game client,
-or an @link {invalid instance | Instance#constants} if they do not exist.
+Returns the Player instance of this game client, <br>
+or `nil` if they do not exist.
 
-On the character select screen in online multiplayer,
+On the character select screen in online multiplayer, <br>
 this will return the local `oPrePlayer`.
 ]]
+---@return Player | Instance | nil
 Player.get_local = function()
-    if Net.online then return Global.my_player end
+    -- TODO
+    -- if Net.online then return Global.my_player end
 
     -- Return first oP to exist
     local inst = gm.instance_find(gm.constants.oP, 0)
-    if inst ~= -4 then return Instance.wrap(inst) end
-    return Instance.INVALID
+    if inst ~= -4 then return inst end
+    return nil
 end
 
 
+-- ========== Wrapper Methods ==========
 
--- ========== Instance Methods ==========
+---@class Player
+local methods = {}
+G.methods_player = methods
 
---@section Instance Methods
+--@instance
+--@return       bool
+--@param        verb        | string    | The verb to check.
+--@optional     type        | number    | <br>`0` - Returns `true` if the verb input is being held. <br>`1` - Returns `true` if the verb input was just pressed. <br>`-1` - Returns `true` if the verb input was just released. <br><br>`0` by default.
+--[[
+Returns the input status for a @link {verb | ModOptionsKeybind}.
+Only returns `true` for the local player, and if the game is not paused.
 
-methods_player = {
-
-    --@instance
-    --@return       bool
-    --@param        verb        | string    | The verb to check.
-    --@optional     type        | number    | <br>`0` - Returns `true` if the verb input is being held. <br>`1` - Returns `true` if the verb input was just pressed. <br>`-1` - Returns `true` if the verb input was just released. <br><br>`0` by default.
-    --[[
-    Returns the input status for a @link {verb | ModOptionsKeybind}.
-    Only returns `true` for the local player, and if the game is not paused.
-
-    For more general uses, use @link {`gm.input_check_*` | ModOptionsKeybind} functions instead.
-    ]]
-    control = function(self, verb, _type)
-        if (not _vanilla_player_verbs[verb]) and (not __custom_verbs_all[verb]) then log.error("control: verb is invalid", 2) end
-        return GM.SO.control(self, nil, verb, _type or 0)
-    end,
-
-}
-
-
-
--- ========== Metatables ==========
-
-local wrapper_name = "Player"
-
-make_table_once("metatable_player", {
-    __index = function(proxy, k)
-        -- Get wrapped value
-        if k == "value" or k == "cinstance" then return __proxy[proxy] end
-        if k == "RAPI" then return wrapper_name end
-        if k == "id" then return metatable_instance.__index(proxy, k) end
-
-        -- Methods
-        if methods_player[k] then
-            return methods_player[k]
-        end
-
-        -- Pass to metatable_actor
-        return metatable_actor.__index(proxy, k)
-    end,
-
-
-    __newindex = function(proxy, k, v)
-        -- Pass to metatable_instance
-        metatable_instance.__newindex(proxy, k, v)
-    end,
-
-
-    __eq = function(proxy, other)
-        return metatable_instance.__eq(proxy, other)
-    end,
-
-    
-    __metatable = "RAPI.Wrapper."..wrapper_name
-})
-
-
-
--- Public export
-__class.Player = Player
+For more general uses, use @link {`gm.input_check_*` | ModOptionsKeybind} functions instead.
+]]
+---@param verb string The verb to check.
+---@param type? number `0` - Returns `true` if the verb input is being held. <br>`1` - Returns `true` if the verb input was just pressed. <br>`-1` - Returns `true` if the verb input was just released. <br>`0` by default.
+---@return boolean
+methods.control = function(self, verb, _type)
+    -- TODO
+    print("control called")
+    -- if (not _vanilla_player_verbs[verb]) and (not __custom_verbs_all[verb]) then log.error("control: verb is invalid", 2) end
+    -- return GM.SO.control(self, nil, verb, _type or 0)
+end
